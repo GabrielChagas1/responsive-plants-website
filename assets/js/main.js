4aa687028d05316b35e0d07b68f46c207b61bb4c
Gabriel Chagas <gabrielserq@outlook.com>
2022-01-31 08:37:44 -0300

    chore: creating js scroll sections active link.

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,10 +65,24 @@ const toggleItem = (item) =>{
         item.classList.add('accordion-open')
     }
 }
+const sections = document.querySelectorAll('section[id]')
 
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
 
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== SHOW SCROLL UP ===============*/ 
 
