37f6a62713dfa54932e738bb5fc331748929706f
Gabriel Chagas <gabrielserq@outlook.com>
2022-01-31 08:39:03 -0300

    chore: creating js scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -65,6 +65,7 @@ const toggleItem = (item) =>{
         item.classList.add('accordion-open')
     }
 }
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
@@ -123,3 +124,17 @@ themeButton.addEventListener('click', () => {
 })
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+});
+
+sr.reveal(`.home__data`)
+sr.reveal(`.home__img`, {delay: 500})
+sr.reveal(`.home__social`, {delay: 600})
+sr.reveal(`.about__img, .contact__box`, {origin: 'left'})
+sr.reveal(`.about__data, .contact__form`, {origin: 'right'})
+sr.reveal(`.steps__card, .product__card, .questions__group, .footer`, {interval: 100})
